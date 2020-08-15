cee101a23a016847a7bb6c5f432d0c10eeebe3b2
anishrajan25 <anishrajan2708@gmail.com>
2020-08-15 14:54:09 +0530

    version 1

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import  Main from "./components/Main";
-import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from "react-redux";
 import { ConfigureStore } from "./redux/configureStore";
 
